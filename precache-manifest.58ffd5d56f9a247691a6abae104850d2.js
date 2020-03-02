1aab189715de6ad27608245f83825fe9c7945b43
Cyro Dubeux <xorycx@gmail.com>
2020-03-02 00:11:13 -0300

    Updates

---
+++ precache-manifest.58ffd5d56f9a247691a6abae104850d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5377d09120ecb7b21ffa9c97c07b2198",
+    "url": "/swapi-app/index.html"
+  },
+  {
+    "revision": "5ad63a6c02473b06d8ec",
+    "url": "/swapi-app/static/css/main.c24205bf.chunk.css"
+  },
+  {
+    "revision": "8391e3f980724fda3585",
+    "url": "/swapi-app/static/js/2.f758448f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/swapi-app/static/js/2.f758448f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ad63a6c02473b06d8ec",
+    "url": "/swapi-app/static/js/main.02b822a9.chunk.js"
+  },
+  {
+    "revision": "b8f1666f8263f44a40f9",
+    "url": "/swapi-app/static/js/runtime-main.d9161730.js"
+  },
+  {
+    "revision": "b0247cc17cce4916c5d6a27b9c3073f5",
+    "url": "/swapi-app/static/media/logo.b0247cc1.png"
+  }
+]);
